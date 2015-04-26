45cfe2a1d739b381ff6cf9cf110afae3721049a0
mharward <mharward@gmail.com>
2015-04-27 11:43:30 +1200

    - Extract versioning into own file and automatically find latest based on highest id.

--- service.js
+++ service.js
@@ -2,72 +2,31 @@
 var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
-	m = require('measurejs'),
-	toXml = require("js2xmlparser"),
 	limiter = require('./lib/rate_limit'),
 	acceptHeader = require('./lib/accept_header'),
+	versioning = require('./lib/versioning'),
 	h = require('./lib/helpers');
 
 var staticPaths = ['/docs', '/static-docs', '/api-docs'];
 var crossVersionPaths = ['/versions', '/status'];
 var port = process.env.PORT || 8080;
 
-// GENERAL MIDDLEWARE
+// MIDDLEWARE
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(limiter.limiterExcludeStatic(staticPaths));
+app.use(acceptHeader(versioning.routes, crossVersionPaths, staticPaths));
 
-// VERSIONING
+// SETUP VERSIONED ENDPOINTS
 
-var routes = {},
-	versions = [],
-	routePath = require("path").join(__dirname, "lib/routes"),
-	latestRouteFileName = 'v1.js';
+app.use(versioning);
 
-// Load routes dynamically
-require("fs").readdirSync(routePath).forEach(function (file) {
-	routes[file] = require("./lib/routes/" + file);
-});
-
-// Utilise accept header versioning
-app.use(acceptHeader(routes, crossVersionPaths, staticPaths));
-
-// Apply routes to express and set up versions object
-for (var routeKey in routes) {
-	var route = routes[routeKey];
-	provisionVersion(routeKey, route.version.name, route.version.path, route.version.accept);
-}
-
-// Apply latest api version
-provisionVersion(latestRouteFileName, 'Latest', '', [
-	'application/json',
-	'application/xml',
-	'application/vnd.measurement+json',
-	'application/vnd.measurement+xml'
-]);
-
-function provisionVersion(key, name, path, accept) {
-	var route = routes[key];
-	versions.push({
-		name: name,
-		path: path,
-		accept: accept,
-	});
-	app.use(path, route);
-}
-
-// Reset Server header for requests that get this far
-app.use(function (req, res, next) {
-	res.set('Server', 'Measurement/v*');
-	next();
-});
+// CROSS-VERSION ENDPOINTS
 
 app.get('/versions', function (req, res) {
-	h.sendResponse(req, res, versions, 'versions');
+	h.sendResponse(req, res, versioning.versions, 'versions');
 });
-
-// CROSS-VERSION FUNCTIONALITY
 
 app.get('/status', function (req, res) {
 	var status = limiter.limitStatus(req);
